d9c0ec6919fc6227b0121ad739410e8b79a92f11
Abir-Dutta <abir.caesar@gmail.com>
2019-06-22 15:41:06 +0530

    more changes

--- assets/js/utility.js
+++ assets/js/utility.js
@@ -2,3 +2,26 @@ function togglepage(tohide,toshow){
     document.getElementsByClassName(tohide)[0].style.display ="none";
     document.getElementsByClassName(toshow)[0].style.display ="block";
 }
+
+var statter_element =[];
+$( document ).ready(function() {
+    $('.roles').click(function(){
+        $('.check').hide();
+        $(this).find('.check').show();
+        $('.sign-up>input').removeClass('disabled');
+        $('.sign-up>input').removeAttr("disabled");
+    })
+
+    $('.scatter-button').click(function(e){
+       var statter_elementidx = statter_element.indexOf(e.target.value)
+        if(statter_elementidx==-1){
+            statter_element.push(e.target.value);
+            $(e.target).addClass('hollow-clicked')
+        }
+        else {
+            statter_element.splice(statter_elementidx,1);
+            $(e.target).removeClass('hollow-clicked')
+        }
+    })
+  });
+
