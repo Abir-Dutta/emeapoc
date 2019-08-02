9dc082a7cb77b9e1d9a5f01c560ee46e793ac84e
Abir-Dutta <abir.caesar@gmail.com>
2019-08-02 17:27:57 +0530

    For  ionic

---
+++ forIonic/NRA/src/app/tabs/tabs.page.ts
@@ -0,0 +1,15 @@
+import { Component } from '@angular/core';
+import {   NavController } from '@ionic/angular';
+
+@Component({
+  selector: 'app-tabs',
+  templateUrl: 'tabs.page.html',
+  styleUrls: ['tabs.page.scss']
+})
+export class TabsPage {
+
+  constructor(public navCtrl: NavController) {}
+  goToSignUp() {
+    this.navCtrl.navigateForward('Tab1Page');
+  }
+}
