6059e8dbeb58566b7150b5be0e74a3e32629c8a8
Abir-Dutta <abir.caesar@gmail.com>
2020-03-01 01:59:36 +0530

    qliksee

---
+++ forIonic/Qliksee/qlikSee/src/app/app-routing.module.ts
@@ -0,0 +1,21 @@
+import { NgModule } from '@angular/core';
+import { PreloadAllModules, RouterModule, Routes } from '@angular/router';
+import { SplashScreenComponent } from './splash-screen/splash-screen.component';
+import { IntroScreenComponent } from './intro-screen/intro-screen.component';
+
+
+const routes: Routes = [
+  // {
+  //   path: '',
+  //   loadChildren: () => import('./splash-screen/splash-screen.component').then(m => m.SplashScreenComponent)
+  // },
+  { path: '', component: SplashScreenComponent },
+  { path: 'introscreen', component: IntroScreenComponent }
+];
+@NgModule({
+  imports: [
+    RouterModule.forRoot(routes, { preloadingStrategy: PreloadAllModules })
+  ],
+  exports: [RouterModule]
+})
+export class AppRoutingModule {}
---
+++ forIonic/Qliksee/qlikSee/src/app/app.module.ts
@@ -0,0 +1,30 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+import { RouteReuseStrategy } from '@angular/router';
+
+import { IonicModule, IonicRouteStrategy } from '@ionic/angular';
+import { SplashScreen } from '@ionic-native/splash-screen/ngx';
+import { StatusBar } from '@ionic-native/status-bar/ngx';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+
+import { SplashScreenComponent } from './splash-screen/splash-screen.component';
+import { IntroScreenComponent } from  './intro-screen/intro-screen.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+     SplashScreenComponent,
+     IntroScreenComponent
+    ],
+  entryComponents: [],
+  imports: [BrowserModule, IonicModule.forRoot(), AppRoutingModule],
+  providers: [
+    StatusBar,
+    SplashScreen,
+    { provide: RouteReuseStrategy, useClass: IonicRouteStrategy }
+  ],
+  bootstrap: [AppComponent]
+})
+export class AppModule {}
